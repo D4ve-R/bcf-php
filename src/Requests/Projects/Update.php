28161299eedb6ce186a4c7354a7072c78cda172e
Dave <69651599+D4ve-R@users.noreply.github.com>
2024-12-10 05:41:59 +0100

    Add UpdateProjectsRequest

---
+++ src/Requests/Projects/Update.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace D4veR\BcfPhp\Requests\Projects;
+
+use Saloon\Contracts\Body\HasBody;
+use Saloon\Enums\Method;
+use Saloon\Traits\Body\HasJsonBody;
+
+
+class UpdateProjectsRequest extends GetProjectsRequest implements HasBody
+{
+    use HasJsonBody;
+
+    protected Method $method = Method::PUT;
+
+    public function __construct(
+        protected string $project_id,
+        protected string $name,
+        protected string $authorization
+    ) {
+        parent::__construct($project_id);
+    }
+
+    public function resolveBody(): array
+    {
+        return [
+            'name' => $this->name,
+            'authorization' => $this->authorization,
+        ];
+    }
+}
