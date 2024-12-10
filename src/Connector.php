24a359af1c125f6b29f401d6caddcb3e406dec55
Dave <69651599+D4ve-R@users.noreply.github.com>
2024-12-10 05:40:22 +0100

    Add Connector

---
+++ src/Connector.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace D4veR\BcfPhp;
+
+use Saloon\Http\Connector as BaseConnector;
+
+class Connector extends BaseConnector
+{
+    public function __construct(
+        protected readonly string $baseUrl,
+        public string $bcfVersion = '3.0',
+    ) {
+        //
+    }
+
+    public function resolveBaseUrl(): string
+    {
+        return $this->baseUrl . '/' . $this->bcfVersion;
+    }
+
+    protected function defaultHeaders(): array
+    {
+        return [
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+        ];
+    }
+}
