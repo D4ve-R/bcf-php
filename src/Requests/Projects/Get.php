96ce48cd63462c83268c09d8508b0ea205161fbd
Dave <69651599+D4ve-R@users.noreply.github.com>
2024-12-10 05:41:41 +0100

    Add GetProjectsRequest

---
+++ src/Requests/Projects/Get.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace D4veR\BcfPhp\Requests\Projects;
+
+use D4veR\BcfPhp\Data\Project;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Response;
+
+class GetProjectsRequest extends BaseRequest
+{
+    protected Method $method = Method::GET;
+
+    public function __construct(
+        protected readonly string $project_id
+    ) {
+        //
+    }
+
+    public function resolveEndpoint(): string
+    {
+        return parent::resolveEndpoint() . '/' . $this->project_id;
+    }
+
+    public function createDtoFromResponse(Response $response): Project
+    {
+        $project = $response->json();
+
+        return new Project(
+            name: $project['name'],
+            project_id: $project['project_id'],
+            authorization: $project['authorization'],
+        );
+    }
+}
