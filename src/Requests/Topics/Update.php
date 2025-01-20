f7621ba8c97fa77ecf13c475f6e0480ea4aedaef
Dave <69651599+D4ve-R@users.noreply.github.com>
2025-01-20 14:20:37 +0100

    add topic update request

---
+++ src/Requests/Topics/Update.php
@@ -0,0 +1,29 @@
+<?php 
+
+namespace D4veR\BcfPhp\Requests\Topics;
+
+use D4veR\BcfPhp\Data\Topic;
+
+use Saloon\Contracts\Body\HasBody;
+use Saloon\Enums\Method;
+use Saloon\Traits\Body\HasJsonBody;
+
+class UpdateTopicRequest extends GetTopicsRequest implements HasBody
+{
+    use HasJsonBody;
+
+    protected Method $method = Method::PUT;
+
+    public function __construct(
+        protected readonly string $project_id,
+        protected readonly string $topic_id,
+        protected readonly Topic $topic
+    ) {
+        parent::__construct($project_id, $topic_id);
+    }
+
+    public function resolveBody()
+    {
+        return $this->topic;
+    }
+}
