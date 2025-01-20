366f4d866785768220b508e45bf963e02f0e0121
Dave <69651599+D4ve-R@users.noreply.github.com>
2025-01-20 14:20:16 +0100

    add topics index request

---
+++ src/Requests/Topics/Index.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace D4veR\BcfPhp\Requests\Topics;
+
+use D4veR\BcfPhp\Data\Topic;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Response;
+
+class IndexTopicsRequest extends BaseRequest
+{
+    protected Method $method = Method::GET;
+
+    public function createDtoFromResponse(Response $response): array
+    {
+        $data = $response->json();
+
+        return array_map(function ($topic) {
+            return Topic::fromArray($topic);
+        }, $data);
+    }
+}
