538a44417ef419758f9f97cfcef2dbbf747099fd
Dave <69651599+D4ve-R@users.noreply.github.com>
2025-01-20 14:10:56 +0100

    add project extensions request

---
+++ src/Requests/Projects/Extensions.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace D4veR\BcfPhp\Requests\Projects;
+
+use D4veR\BcfPhp\Data\ProjectExtensions;
+use Saloon\Enums\Method;
+use Saloon\Http\Response;
+
+class ExtensionsProjectsRequest extends BaseRequest
+{
+    public function __construct(
+        protected string $project_id,
+        protected string $name,
+        protected string $authorization
+    ) {
+        parent::__construct($project_id);
+    }
+
+    public function resolveEndpoint(): string
+    {
+        return parent::resolveEndpoint() . '/' . $this->project_id . '/extensions';
+    }
+
+    public function createDtoFromResponse(Response $response): ProjectExtensions
+    {
+        $extensions = $response->json();
+
+        return ProjectExtensions::fromArray($extensions);
+    }
+}
