ef2b77335d40d1c56c1f6885419e1291431f9f67
Dave <69651599+D4ve-R@users.noreply.github.com>
2025-01-20 15:08:59 +0100

    add project events request

---
+++ src/Requests/Events/Project.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace D4veR\BcfPhp\Requests\Projects;
+
+use D4veR\BcfPhp\Data\Event;
+use Saloon\Enums\Method;
+use Saloon\Http\Response;
+
+class ProjectRequest extends BaseRequest
+{
+    public function resolveEndpoint(): string
+    {
+        return parent::resolveEndpoint() . '/events';
+    }
+
+    public function createDtoFromResponse(Response $response): array
+    {
+        $events = $response->json();
+
+        return array_map(function ($event) {
+            return Event::fromArray($event);
+        }, $events);
+    }
+}
