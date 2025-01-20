085ad044667c39bc6fb58b37ee69f80d9cb6d16d
Dave <69651599+D4ve-R@users.noreply.github.com>
2025-01-20 14:20:25 +0100

    add topics get request

---
+++ src/Requests/Topics/Get.php
@@ -0,0 +1,33 @@
+<?php 
+
+namespace D4veR\BcfPhp\Requests\Topics;
+
+use D4veR\BcfPhp\Data\Topic;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Response;
+
+
+class GetTopicsRequest extends BaseRequest
+{
+    protected Method $method = Method::GET;
+
+    public function __construct(
+        protected readonly string $project_id,
+        protected readonly string $topic_id
+    ) {
+        parent::__construct($project_id);
+    }
+
+    public function resolveEndpoint(): string
+    {
+        return parent::resolveEndpoint() . '/' . $this->topic_id;
+    }
+
+    public function createDtoFromResponse(Response $response): Topic
+    {
+        $topic = $response->json();
+
+        return Topic::fromArray($topic);
+    }
+}
