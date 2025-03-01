4ef7dc3bee8bf2c12289c82039f984680ab1bb93
Dave <69651599+D4ve-R@users.noreply.github.com>
2025-03-01 05:50:40 +0100

    Add BimSnippet DTO

---
+++ src/Data/BimSnippet.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace D4veR\BcfPhp\Data;
+
+class BimSnippet {
+    public function __construct(
+        public readonly string $reference,
+        public readonly string $reference_schema,
+        public readonly string $snippet_type,
+        public bool $is_external = false,
+    ) {
+        //
+    }
+
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            reference: $data['reference'],
+            reference_schema: $data['reference_schema'],
+            snippet_type: $data['snippet_type'],
+            is_external: $data['is_external'] ?? false,
+        );
+    }
+}
